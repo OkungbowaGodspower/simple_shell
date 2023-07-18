9c55c05cfb87aaf9bb7f149d12aac4823279c3a4
Okungbowa Godspower <godspowerokungbowa0@gmail.com>
2023-07-18 15:09:33 +0100

    Added functionality to handle user input by implementing an input_reader function that takes a starting index and returns a string of user input up to a maximum length

--- main.h
+++ main.h
@@ -5,7 +5,12 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#define MAX_INPUT_LENGTH 100
 
 void prompt(void);
+char *input_reader(int start);
 
 #endif
