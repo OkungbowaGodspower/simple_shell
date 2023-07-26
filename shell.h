33c84f4a4d8b8d53a5de87cde052c685a68caafb
Okungbowa Godspower <godspowerokungbowa0@gmail.com>
2023-07-27 00:07:25 +0100

    This file contains function declarations for various shell functionalities such as parsing input, executing commands, handling logical operators, and managing environment variables

---
+++ shell.h
@@ -0,0 +1,27 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <limits.h>
+
+void run_shell(void);
+char *get_input(void);
+char **strsplit(const char *str, const char *delim);
+void free_tokens(char **tokens);
+int count_tokens(char **tokens);
+void execute_command(char **args);
+void handle_logical_operators(char *line);
+void *safe_malloc(size_t size);
+char *safe_strdup(const char *str);
+void execute_setenv(char **args);
+void execute_env(void);
+void execute_cd(char **args);
+void execute_unsetenv(char **args);
+char* get_current_directory(void);
+
+#endif /* SHELL_H */
