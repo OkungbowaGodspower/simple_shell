6d99e7faafd9bbac61a2bd9f3468e553633e1303
Okungbowa Godspower <godspowerokungbowa0@gmail.com>
2023-07-27 00:31:19 +0100

    Add function to handle the main loop for running the custom shell

---
+++ parse.c
@@ -0,0 +1,70 @@
+#include "shell.h"
+
+/**
+  * run_shell - Main loop for running the custom shell
+  *
+  * Return: returns nothing
+  */
+
+void run_shell(void)
+{
+	int idx = 0;
+	char *line;
+	char **commands;
+
+	while (1)
+	{
+		if (isatty(STDIN_FILENO))
+		{
+			printf("my_shell$ ");
+			fflush(stdout);
+		}
+
+		line = get_input();
+		commands = strsplit(line, ";");
+
+		while (commands[idx] != NULL)
+		{
+			char **args = strsplit(commands[idx], " \t");
+
+			execute_command(args);
+			free_tokens(args);
+			idx++;
+		}
+
+		free(line);
+		free_tokens(commands);
+	}
+}
+
+/**
+  * get_input - read user input from standard input
+  *
+  * Return: a pointer to a dynamically allocated string
+  */
+
+char *get_input()
+{
+	char *line = NULL;
+
+	size_t bufsize = 0;
+
+	ssize_t nread = getline(&line, &bufsize, stdin);
+
+	if (nread == -1)
+	{
+		if (isatty(STDIN_FILENO))
+		{
+			printf("\n");
+		}
+		free(line);
+		exit(0);
+	}
+
+	if (line[nread - 1] == '\n')
+	{
+		line[nread - 1] = '\0';
+	}
+
+	return (line);
+}
