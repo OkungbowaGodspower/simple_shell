1a2504d1415df86cacbc090156c85cc482b8a872
Okungbowa Godspower <godspowerokungbowa0@gmail.com>
2023-07-29 18:15:40 +0100

    Refactor handle_logical_operators to include logical operator functionality

--- operators.c
+++ operators.c
@@ -11,17 +11,51 @@
 void handle_logical_operators(char *line)
 {
 	char **commands = strsplit(line, ";");
-
-	int idx = 0;
+	int idx = 0, next_idx;
 
 	while (commands[idx] != NULL)
 	{
 		char **args = strsplit(commands[idx], " \t");
 
-		execute_command(args);
+		if (strcmp(args[0], "cd") == 0)
+			execute_cd(args);
+		else
+		{
+			int status;
+			pid_t pid = fork();
+
+			if (pid == 0)
+			{
+				if (execvp(args[0], args) == -1)
+				{
+					perror("execute_command");
+					exit(EXIT_FAILURE);
+				}
+			}
+			else if (pid < 0)
+			{
+				perror("fork");
+				exit(EXIT_FAILURE);
+			}
+			else
+			{
+				waitpid(pid, &status, 0);
+				next_idx = idx + 1;
+				if (commands[next_idx] != NULL)
+				{
+					char *logical_op = commands[next_idx];
+					if (WIFEXITED(status))
+					{
+						if (WEXITSTATUS(status) == 0 && strcmp(logical_op, "&&") == 0)
+							idx++;
+						else if (WEXITSTATUS(status) != 0 && strcmp(logical_op, "||") == 0)
+							idx++;
+					}
+				}
+			}
+		}
 		free_tokens(args);
 		idx++;
 	}
-
 	free_tokens(commands);
 }
