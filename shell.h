f8815ff086c01b64463ec1bfa0739f168d028c24
Okungbowa Godspower <godspowerokungbowa0@gmail.com>
2023-08-09 15:01:51 +0100

    Revert "Add alias support to the shell"
    This reverts commit 7dd3499138804878e5534b973c9920158c8cbc71.

--- shell.h
+++ shell.h
@@ -11,14 +11,6 @@
 
 extern char **environ;
 
-#define MAX_ALIASES 50
-
-typedef struct Alias
-{
-	char *name;
-	char *value;
-} Alias;
-
 void run_shell(void);
 char *get_input(void);
 char **strsplit(const char *str, const char *delim);
@@ -31,6 +23,11 @@ char *safe_strdup(const char *str);
 void execute_setenv(char **args);
 void execute_env(void);
 void execute_cd(char **args);
+void execute_alias(char **args);
+void print_all_aliases(void);
+char *get_alias_value(const char *name);
+void add_alias(const char *name, const char *value);
+void update_alias(const char *name, const char *value);
 void execute_unsetenv(char **args);
 char *get_current_directory(void);
 void execute_exit(char **args);
