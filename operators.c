a1d39969f1e5a207adef7de9fd26efbd413303bc
Okungbowa Godspower <godspowerokungbowa0@gmail.com>
2023-08-09 14:58:26 +0100

    Revert "Refactor handle_logical_operators to include logical operator functionality"
    This reverts commit 1a2504d1415df86cacbc090156c85cc482b8a872.

--- operators.c
+++ operators.c
@@ -11,51 +11,17 @@
 void handle_logical_operators(char *line)
 {
 	char **commands = strsplit(line, ";");
-	int idx = 0, next_idx;
+
+	int idx = 0;
 
 	while (commands[idx] != NULL)
 	{
 		char **args = strsplit(commands[idx], " \t");
 
-		if (strcmp(args[0], "cd") == 0)
-			execute_cd(args);
-		else
-		{
-			int status;
-			pid_t pid = fork();
-
-			if (pid == 0)
-			{
-				if (execvp(args[0], args) == -1)
-				{
-					perror("execute_command");
-					exit(EXIT_FAILURE);
-				}
-			}
-			else if (pid < 0)
-			{
-				perror("fork");
-				exit(EXIT_FAILURE);
-			}
-			else
-			{
-				waitpid(pid, &status, 0);
-				next_idx = idx + 1;
-				if (commands[next_idx] != NULL)
-				{
-					char *logical_op = commands[next_idx];
-					if (WIFEXITED(status))
-					{
-						if (WEXITSTATUS(status) == 0 && strcmp(logical_op, "&&") == 0)
-							idx++;
-						else if (WEXITSTATUS(status) != 0 && strcmp(logical_op, "||") == 0)
-							idx++;
-					}
-				}
-			}
-		}
+		execute_command(args);
 		free_tokens(args);
 		idx++;
 	}
+
 	free_tokens(commands);
 }
